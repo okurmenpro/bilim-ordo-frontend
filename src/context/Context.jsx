ac429534960b773b9db8655bb6fa752b64be6ec8
Islam <gojo060809@gmail.com>
2024-09-24 16:36:26 +0600

    add context

---
+++ src/context/Context.jsx
@@ -0,0 +1,13 @@
+import React, { createContext } from 'react'
+
+export const DataContext = createContext()
+
+const Context = ({ children }) => {
+
+    const value = {}
+      return (
+        <DataContext.Provider value={value}>{children}</DataContext.Provider>
+      )
+}
+
+export default Context
