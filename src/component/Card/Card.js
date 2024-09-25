7f876d9b2db1d456559f3f1d4d7d710a95d79fc3
ваше имя <email@example.com>
2024-09-25 11:02:05 +0300

    bootstrap

---
+++ src/component/Card/Card.js
@@ -0,0 +1,23 @@
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import './App.scss';
+/* The following line can be included in a src/App.scss */
+@import '~bootstrap/scss/bootstrap';
+
+function TypesExample() {
+  return (
+    <>
+      <Button variant="primary">Primary</Button>{' '}
+      <Button variant="secondary">Secondary</Button>{' '}
+      <Button variant="success">Success</Button>{' '}
+      <Button variant="warning">Warning</Button>{' '}
+      <Button variant="danger">Danger</Button>{' '}
+      <Button variant="info">Info</Button>{' '}
+      <Button variant="light">Light</Button>{' '}
+      <Button variant="dark">Dark</Button>
+      <Button variant="link">Link</Button>
+    </>
+  );
+}
+
+export default TypesExample;
