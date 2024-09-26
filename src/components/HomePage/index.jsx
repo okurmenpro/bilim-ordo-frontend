b73eaed208a93569589507460e403d2282a8f153
Nurtilek <nuke.ff13@gmail.com>
2024-09-26 18:05:17 +0600

    cart-pro

--- src/components/HomePage/index.jsx
+++ src/components/HomePage/index.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Footer from '../Footer'
 import Banner from '../Banner'
 import Header from '../Header'
+import Card from '../Card'
 
 function HomePage() {
   return (
     <div>
       <Header />
       <Banner />
+      <Card />
       <Footer />
     </div>
   )
