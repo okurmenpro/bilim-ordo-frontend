3464bcd44227fc1d8208bea3377bb6274e176e07
kulpunai1808 <jekshenbekova@outlook.com>
2024-09-26 14:27:18 +0600

    BannerCourse

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 // import logo from './logo.svg';
 import './App.css';
 import BannerCom from './components/Banner/BannerCom';
+import Banner from './components/bannerCourse/Banner';
 
 function App() {
   return (
     <div className="App">
       <BannerCom/>
+      <Banner/>
     </div>
   );
 }
--- src/components/Banner/BannerCom.jsx
+++ src/components/Banner/BannerCom.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import './BannerCom.css'
 // import Carousel from 'react-bootstrap/Carousel';
-import Button from 'react-bootstrap/Button';
+// import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Slider from '../slider';
 // import ExampleCarouselImage from 'components/ExampleCarouselImage';
-// import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 function BannerCom() {
