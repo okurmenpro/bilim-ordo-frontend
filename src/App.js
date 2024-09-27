2453b0f3ef0f0a2f06a9d83da7b50f2fac19967b
Tangsuluu <karybekovatansuluu303@gmail.com>
2024-09-27 22:19:01 +0600

    dropdown

--- src/App.js
+++ src/App.js
@@ -1,14 +1,6 @@
-// import logo from './logo.svg';
 import './App.css';
-import DropdownProfile from './Dropdown/DropdownProfile';
-
-function App() {
-  return (
-    <div>
-     <DropdownProfile/>
-import './App.css';
-import { RouterProvider } from 'react-router-dom'
-import { Router } from './router'
+import { RouterProvider } from 'react-router-dom';
+import { Router } from './router';
 
 function App() {
   return (
