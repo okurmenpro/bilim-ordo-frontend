cea94d7458192818ed1cafc663177941fde82613
MahmudovIbragim <ibrahimorunbaev59@gmail.com>
2024-09-30 17:14:35 +0600

    bugfix navbar links

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,37 +1,49 @@
-import React from 'react'
-import "./Header.scss"
-import udemy from "../../assets/images/udemy.png"
-import heart from "../../assets/svg/heart.svg"
-import korzina from "../../assets/svg/korzina.svg"
-import notif from "../../assets/svg/notif.svg"
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from "react";
+import "./Header.scss";
+import udemy from "../../assets/images/udemy.png";
+import heart from "../../assets/svg/heart.svg";
+import korzina from "../../assets/svg/korzina.svg";
+import notif from "../../assets/svg/notif.svg";
+import { Link } from "react-router-dom";
 
 function Header() {
-  const categories = ['Разработка', 'Бизнес', 'Финансы и бухгалтерский учет', 'ИТ и ПО', 'Офисное Программное обеспечение']
+  const categories = [
+    "Веб-дизайн ",
+    "Графический дизайн и иллюстрация",
+    "Инструменты дизайна",
+    "Дизайн пользовательского интерфейса",
+    "Дизайн игр",
+  ];
 
-  const providers = [ 'Udemy Business', 'Преподавайте на Udemy','Мое обучение']
+  const providers = ["Udemy Business", "Преподавайте на Udemy", "Мое обучение"];
   return (
     <header>
-        <div class="container">
-            <img src={udemy} alt="" />
-           <h3>Категории</h3>
-           <input type="search" placeholder='Ищите что угодно' />
+      <div class="container">
+        <img src={udemy} alt="" />
+        <h3>Категории</h3>
+        <input type="search" placeholder="Ищите что угодно" />
 
-           <div className='providers'>
-            {providers.map((provider) => <a>{provider}</a>)}
-            </div> 
+        <div className="providers">
+          {providers.map((provider) => (
+            <a href="#">{provider}</a>
+          ))}
+        </div>
 
-            <div className='icons'>
-                <img src={heart} alt="" />
-                <img src={korzina} alt="" />
-                <img className='not' src={notif} alt="" />
-            </div>
+        <div className="icons">
+          <img src={heart} alt="" />
+          <img src={korzina} alt="" />
+          <img className="not" src={notif} alt="" />
         </div>
-        <hr />
-        <div className='bottom container'>
-          {categories.map((category) => <li>{category}</li>)}
-        </div>
+      </div>
+      <hr />
+      <div className="bottom container">
+          {categories.map((category) => (
+            <Link to="#">{category}</Link>
+          ))}
+      </div>
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
