c1ebba69fe648f83b4285d1d0b0caa21eaf672c9
bakytbekov1ch <kalmamatovelibek32@gmail.com>
2024-10-01 15:59:23 +0600

    chore: img

--- src/components/Course/Course.jsx
+++ src/components/Course/Course.jsx
@@ -8,7 +8,7 @@ function Course({ image, name, text, avtor, month, price }) {
     <div className="course">
       <div className="wrapper">
         <div className="course__content">
-          <img src={image} alt="" />
+          <img src={image} alt="photo to video to kurs" />
 
           <div className="course__text">
             <h1>{name}</h1>
@@ -22,10 +22,10 @@ function Course({ image, name, text, avtor, month, price }) {
 
             <div className="course__star">
               <h4>4,7</h4>
-              <img src={star} alt="" />
-              <img src={star} alt="" />
-              <img src={star} alt="" />
-              <img src={star} alt="" />
+              <img src={star} alt="star" />
+              <img src={star} alt="star" />
+              <img src={star} alt="star" />
+              <img src={star} alt="star" />
               <h5>(15)</h5>
             </div>
 
