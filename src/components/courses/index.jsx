fccb33fbd5d7dfa044bd03e6d2200bd2d4dcc34e
Nurtilek <nuke.ff13@gmail.com>
2024-10-08 14:56:33 +0600

    courses

--- src/components/courses/index.jsx
+++ src/components/courses/index.jsx
@@ -1,45 +1,70 @@
-import React from 'react'
-import "./Courses.css"
+import React from 'react';
+import "./Courses.css";
 import { MdOutlineStarPurple500 } from "react-icons/md";
 
 
-function Сourses() {
+
+function Courses() {
+  const coursesData = [
+    {
+      id: 1,
+      image: "https://d3f1iyfxxz8i1e.cloudfront.net/courses/course_image/3cfbe3220824.jpg",
+      title: "Полное руководство по Python 3: от новичка до специалиста",
+      price: 13.99,
+      oldPrice: 74.99,
+      description: "Изучи Python 3 с нуля - один из самых популярных языков программирования в мире + Введение в SQL и PostgreSQL",
+      author: "Илья Фофанов, DevSchool Progressive Training Solutions",
+      rating: 4.7,
+      reviews: 9888,
+      totalHours: "Всего 39ч 217 лекций. Начальный",
+    },
+    {
+      id: 2,
+      image: "https://d3f1iyfxxz8i1e.cloudfront.net/courses/course_image/3cfbe3220824.jpg", 
+      title: "Полное руководство по Python 3: от новичка до специалиста",
+      price: 13.99,
+      oldPrice: 74.99,
+      description: "Изучи Python 3 с нуля - один из самых популярных языков программирования в мире + Введение в SQL и PostgreSQL",
+      author: "Илья Фофанов, DevSchool Progressive Training Solutions",
+      rating: 4.7,
+      reviews: 9888,
+      totalHours: "Всего 39ч 217 лекций. Начальный",
+    },
+  ];
   return (
     <div>
-      <div className='courses'>
-      <div className='courses-left'>
-        <img src="https://d3f1iyfxxz8i1e.cloudfront.net/courses/course_image/3cfbe3220824.jpg" alt="пайтон" />
-      </div>
-      <div className='courses-right'>
-        <div className='courses-h1'>
-          <h1>Полное руководство по Python 3: от новичка до специалиста</h1>
-          <h3>13,99$</h3>
+      {coursesData.map(course => (
+        <div key={course.id} className='courses'>
+          <div className='courses-left'>
+            <img src={course.image} alt={course.title} />
+          </div>
+          <div className='courses-right'>
+            <div className='courses-h1'>
+              <h1>{course.title}</h1>
+              <h3>{course.price}$</h3>
+            </div>
+
+            <div className='courses-p'>
+              <p>{course.description}</p>
+              <h3>{course.oldPrice}$</h3>
+            </div>
+
+            <div className='courses-3text'>
+              <h4>{course.author}</h4>
+              <div className='courses-flex'>
+                <h3>{course.rating}</h3>
+                {[...Array(Math.floor(course.rating))].map((_, index) => (
+                  <MdOutlineStarPurple500 key={index} className='star-5' />
+                ))}
+                <p>({course.reviews})</p>
+              </div>
+              <h5>{course.totalHours}</h5>
+            </div>
+          </div>
         </div>
-
-        <div className='courses-p'>
-          <p>Изучи Python 3 с нуля - один из самых популярных языков программирования в мире + Введение в SQL и PostgreSQL</p>
-          <h3>74,99$</h3>
-        </div>
-
-        <div className='courses-3text'>
-
-      <h4>Илья Фофанов, DevSchool Progressive Training Solutions</h4>
-
-        <div className='courses-flex'>
-          <h3>4,7</h3>
-
-          <MdOutlineStarPurple500 className='star-5' />         <MdOutlineStarPurple500 className='star-5' />       <MdOutlineStarPurple500 className='star-5' />       <MdOutlineStarPurple500 className='star-5' />       <MdOutlineStarPurple500 className='star-5' />   
-
-          <p>(9 888)</p>
-        </div>
-        <h5>Всего 39ч 217 лекций. Начальный</h5>
-
-        </div>
-
-      </div>
-      </div>
+      ))}
     </div>
-  )
+  );
 }
 
-export default Сourses
+export default Courses;
