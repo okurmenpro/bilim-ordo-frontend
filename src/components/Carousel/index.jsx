66899a39cdad5648d7f1393e00a53bde5915e043
alisher <alisermanasov00@gmail.com>
2024-10-10 14:58:59 +0600

    banner-fixed 2

--- src/components/Carousel/index.jsx
+++ src/components/Carousel/index.jsx
@@ -1,27 +1,21 @@
-import {Carousel} from 'react-bootstrap';
-
+import { useEffect, useState } from 'react';
+import { Carousel } from 'react-bootstrap';
+import slides from './Carousel.json'; 
 
 function Slider() {
+  const [data, setData] = useState(slides); 
   return (
-    <Carousel className='banner-container container'> 
-      <Carousel.Item className='banner-in'>
-        <img src="https://img-c.udemycdn.com/notices/web_carousel_slide/image/e6cc1a30-2dec-4dc5-b0f2-c5b656909d5b.jpg" alt="" />
-        <Carousel.Caption>
-          <h3>First Okurmen Project </h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-
-      <Carousel.Item className='banner-in'>
-      <img src="https://img-c.udemycdn.com/notices/web_carousel_slide/image/10ca89f6-811b-400e-983b-32c5cd76725a.jpg" alt="" />
-        <Carousel.Caption>
-          <h3>Second Okurmen Project </h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
+    <Carousel className='banner-container container'>
+      {data.map((slide, index) => (
+        <Carousel.Item key={index} className='banner-in'>
+          <img src={slide.imgSrc} alt={slide.title} />
+          <Carousel.Caption>
+            <h3>{slide.title}</h3>
+            <p>{slide.description}</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      ))}
     </Carousel>
-
-
   );
 }
 
