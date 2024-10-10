ca3b1b3be47c47b24596b31fc96104c3cb115f6f
Nurtilek <nurtilekmomunov99@gmail.com>
2024-10-10 18:46:13 +0600

    sidebar класстар туурланды

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -3,7 +3,7 @@ import "./Sidebar.css"
 import { ImAlarm } from "react-icons/im";
 import { SidebarData } from '../../data/Sidebar';
 import { HiMiniXMark } from "react-icons/hi2";
-
+import { Link } from 'react-router-dom';
 function Sidebar({ price, originalPrice, discount, daysLeft, coupon, videoSrc }) {
     return (
         <div className='  container1 Sidebar'>
@@ -13,13 +13,13 @@ function Sidebar({ price, originalPrice, discount, daysLeft, coupon, videoSrc })
 
             <div className='list5'>
                 <div className='price'>
-                    <h1>{price}$</h1>
-                    <h2>{originalPrice}$</h2>
-                    <h3>Скидка {discount}%</h3>
+                    <div className="price-value">{price}$</div>
+                    <div className='original-price'>{originalPrice}$</div>
+                    <div className='discount-div'>Скидка {discount}%</div>
                 </div>
                 <div className='day'>
                     <ImAlarm className='watch' />
-                    <h1>Данная цена доступна eще  <a href="">{daysLeft}дней!</a></h1>
+                    <div className='watch-text'>Данная цена доступна eще  <a href="">{daysLeft}дней!</a></div>
                 </div>
 
                 <button className='add-btn'>Добавить в корзину</button>
@@ -30,35 +30,35 @@ function Sidebar({ price, originalPrice, discount, daysLeft, coupon, videoSrc })
 
                 <div className='discount'>
                     <div className='discount_price'>
-                        <h1>Дополнительная <br /> скидка 15%  <a href="">курсы </a> </h1>
-                        <h2 className='bonus'>15BONUS24DAY10NLY</h2>
+                        <span>Дополнительная <br /> скидка 15%  <a href="">курсы </a> </span>
+                        <div className='bonus'>15BONUS24DAY10NLY</div>
                     </div>
-                    <p className='apply'>Применить</p>
+                    <button className='apply'>Применить</button>
+
                 </div>
 
                 <div className='discont__course'>
-                    <h1>Этот курс включает:</h1>
+                    <h3>Этот курс включает:</h3>
 
                     <div className='discont__map1'>
                         {SidebarData.map((item) => (
-
-                            <div key={item.id} className='discont__map'>
-                                <img src={item.img} alt="" />
-                                <h3>{item.title}</h3>
+                            <div key={item.id}     className='discont__map'>
+                                <img src={item.img} alt={item.title} />
+                                <h5>{item.title}</h5>
                             </div>
                         ))}
                     </div>
                 </div>
                 <div className='share_it'>
-                    <h1 className='share__it-title'>Поделиться</h1>
-                    <h2 className='share__it-title'>Подарить этот курс</h2>
+                    <button>Поделиться</button>
+                    <Link to="/path/to/course" className="share_it-title">Подарить этот курс</Link>
                 </div>
-                <h2 className='share__it-title'>Использовать купон</h2>
+                <span>Использовать купон</span>
 
                 <div className='used_coupon'>
-                    <h3>Использован купон <a href="">{coupon}</a><br />
-                        Купон Udemy</h3>
-                    <HiMiniXMark className='icons_x' />
+                    <p>Использован купон <a href="">{coupon}</a><br />
+                        Купон Udemy</p>
+                    <button className='icons_x'> <HiMiniXMark /></button>
                 </div>
 
                 <div className='enter_coupon'>
@@ -67,7 +67,7 @@ function Sidebar({ price, originalPrice, discount, daysLeft, coupon, videoSrc })
                 </div>
                 <div className='line'></div>
                 <div className='training'>
-                    <h2>Планируете обучение 5 или<br /> более человек?</h2>
+                    <h4>Планируете обучение 5 или<br /> более человек?</h4>
                     <p>Получите доступ к более чем 27 000 лучших курсов на Udemy для своей команды — когда угодно, где угодно.</p>
                 </div>
                 <button className='try_btn'>Попробуйте Udemy Business</button>
