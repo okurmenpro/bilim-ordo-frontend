5e8fd9f832ef23c28a0e5f8878a07ee6c0a53917
alisher <alisermanasov00@gmail.com>
2024-10-10 15:23:36 +0600

    banner fixed 3

--- src/pages/Course.jsx
+++ src/pages/Course.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
 import Index from '../components/MaterialCourse/Index'
-// import Banner from '../components/BannerCourse/Banner'
 
 function Course() {
   return (
     <div>
-      {/* <Banner/> */}
       <Index/>
     </div>
   )
