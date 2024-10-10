fd63567b00bd47d7c7b367c3054e4b2fb098ea6e
Tangsuluu <karybekovatansuluu303@gmail.com>
2024-10-10 18:18:02 +0600

    change addcart

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,47 +1,113 @@
 import React, { useState, useRef, useContext } from "react";
 import "./Header.scss";
 import udemy from "../../assets/images/udemy.png";
+import heart from "../../assets/svg/heart.svg";
+import korzina from "../../assets/svg/korzina.svg";
+import notif from "../../assets/svg/notif.svg";
+import wold from "../../assets/svg/wold.svg";
+import { Dropdown } from "react-bootstrap";
+import { MdKeyboardArrowRight } from "react-icons/md";
 import { Link } from "react-router-dom";
 import { CartContext } from "../CartContext";
 import { GrCart } from "react-icons/gr";
-import wold from "../../assets/svg/wold.svg";
-import DropdownMenu from "../Dropdown/Index";
-
 
 function Header() {
+  const [showMainMenu, setShowMainMenu] = useState(false);
+  const [subMenuItems, setSubMenuItems] = useState([]);
+  const [activeMainItem, setActiveMainItem] = useState(null);
   const { cartItems } = useContext(CartContext);
-  const providers = ['Udemy Business', 'Преподавайте на Udemy', 'Мое обучение']
+  
+  const timerRef = useRef(null);
+
+  const mainMenuItems = [
+    { label: "Разработка", subMenu: ["Frontend", "Backend"] },
+    { label: "Бизнес", subMenu: ["Маркетинг", "Продажи"] },
+    { label: "Финансы и бухгалтерский учет", subMenu: ["Бухгалтерия", "Финансовый анализ"] },
+    { label: "ИТ и ПО", subMenu: ["Тестирование", "Программирование"] },
+  ];
+
+  const handleMouseEnterMainMenu = (subMenu, mainItem) => {
+    clearTimeout(timerRef.current);
+    setSubMenuItems(subMenu);
+    setShowMainMenu(true);
+    setActiveMainItem(mainItem);
+  };
+
+  const handleMouseLeaveAllMenus = () => {
+    timerRef.current = setTimeout(() => {
+      setShowMainMenu(false);
+      setActiveMainItem(null);
+    }, 300);
+  };
+
+  const providers = ['Udemy Business', 'Преподавайте на Udemy'];
 
   return (
-    <header >
+    <header>
       <div className="he">
-        <div class="container header">
+        <div className="container header">
           <img src={udemy} alt="" />
-          <DropdownMenu/>
-        <input type="search" placeholder="Ищите что угодно" />
-        <div className='providers'>
-          <Link to={"/"}>
-            {providers.map((provider, index) => (
-              <Link to='#' key={index}>{provider}</Link>
-            ))}
-          </Link>
+          <Dropdown
+            onMouseEnter={() => setShowMainMenu(true)}
+            onMouseLeave={handleMouseLeaveAllMenus}
+          >
+            <button className="cate-btn">Категории</button>
+            {showMainMenu && (
+              <div className="main-menu" onMouseLeave={handleMouseLeaveAllMenus}>
+                {mainMenuItems.map((item, index) => (
+                  <div
+                    key={index}
+                    className={`menu-item ${activeMainItem === item.label ? "active" : ""}`}
+                    onMouseEnter={() => handleMouseEnterMainMenu(item.subMenu, item.label)}
+                  >
+                    <Dropdown.Item id="text" href={`#/action-${index}`}>
+                      {item.label}
+                    </Dropdown.Item>
+                    <MdKeyboardArrowRight />
+                  </div>
+                ))}
+                <div className='providers'>
+                  {providers.map((provider) => <a key={provider}>{provider}</a>)}
+                </div>
 
-          <Link to={"/addcart"}>
-            <div className="cart-icon">
-              <GrCart  className="cartIcon"/>
-              <span className="cart-count">{cartItems.length}</span> 
-            </div>
+                <input type="search" placeholder='Поиск' />
+
+                <div className="login-btns">
+                  <button className="login-btn">Войти</button>
+                  <button className="register-btn">Зарегистрироваться</button>
+                </div>
+
+                <div className='icons'>
+                  <div className="icon">
+                    <img src={korzina} alt="" />
+                    <img src={heart} alt="" />
+                  </div>
+                </div>
+              </div>
+            )}
+          </Dropdown>
+          <input type="search" placeholder="Ищите что угодно" />
+          <div className='providers'>
+            <Link to={"/"}>
+              {providers.map((provider, index) => (
+                <Link to='#' key={index}>{provider}</Link>
+              ))}
+            </Link>
+
+            <Link to={"/basket"}>
+              <div className="cart-icon">
+                <GrCart className="cartIcon" />
+                {cartItems.length > 0 && <span className="cart-count">{cartItems.length}</span>}
+              </div>
             </Link>
             <div className="icon">
               <img className='not' src={wold} alt="" />
             </div>
           </div>
-
         </div>
       </div>
 
       <div className='bottom container'>
-      {/* {categories.map((category) => <li>{category}</li>)}  */}
       </div>
     </header>
   );
