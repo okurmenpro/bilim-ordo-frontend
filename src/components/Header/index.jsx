27d2961dd790e4ecd6bd303b2f5b6eaf0ecb6791
saadat <devilwq99@gmail.com>
2024-10-10 19:05:39 +0600

    again hovers

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,19 +3,19 @@ import "./Header.scss";
 import cart from '../../assets/svg/cart.svg'
 import earth from '../../assets/svg/earth.svg'
 import { GrSearch } from "react-icons/gr";
-
 function Header() {
-  const providers = ['Категории','Udemy Business', 'Преподавайте на Udemy']
-  const authorization = ['Войти' , 'Зарегистрироваться']
+  const providers = ['Udemy Business', 'Преподавайте на Udemy']
   return (
-    <main className="aback">
-      <header className="head container">
-      <h2><span>B</span>ILIM-ORDO</h2>
-        <div className="buttons">
-        {providers.map((provider) =>
-          <button id="active-btn">{provider}</button>)}
-        </div>
+    <div className="aback">
 
+      <header className="head container">
+        <div>
+          <h2><span>B</span>ILIM-ORDO</h2>
+        </div>
+        <div className="buttons">
+          {providers.map((provider) =>
+            <button id="active-btn">{provider}</button>)}
+        </div>
         <div id="search">
           <div class="input-group mb-3">
             <input class="form-control" placeholder="Search" aria-label="Recipient's username" aria-describedby="basic-addon2" />
@@ -26,17 +26,16 @@ function Header() {
             </div>
           </div>
         </div>
-
         <div className="buttons">
-          {authorization.map((auth) =>
-          <button className="login">{auth}</button>)}
+          <button className="login">Войти</button>
+          <button className="sign-up">Зарегистрироваться</button>
         </div>
         <div className="icons">
-            <img src={cart} alt="" />
+          <img src={cart} alt="" />
           <img src={earth} alt="" />
         </div>
       </header>
-    </main>
+    </div>
   );
 }
 export default Header;
