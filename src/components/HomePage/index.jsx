ca3d1822808266bc40d898f7de2793ed7024aa21
ваше имя <email@example.com>
2024-10-10 15:38:06 +0300

    TeachSection

--- src/components/HomePage/index.jsx
+++ src/components/HomePage/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Banner from '../Banner'
 import Courses from "../courses"
+import TeachSection from '../TeachSection'
 
 
 import Card from '../Card'
@@ -14,6 +15,7 @@ function HomePage() {
       <Courses />
       <Card />
       <PopularCategories/>
+      <TeachSection/>
     </div>
   )
 }
