23b309da8e450d50078077044f2597b374c71529
Nurtilek <nuke.ff13@gmail.com>
2024-10-12 19:00:15 +0600

    ошибканы ондодум h2

--- src/components/HomePage/index.jsx
+++ src/components/HomePage/index.jsx
@@ -4,12 +4,9 @@ import Banner from '../Banner'
 import Courses from "../courses"
 import Companystatistics from "../Companystatistics"
 
-import Card from '../Card'
-import PopularCategories from '../popularCategories';
 
-import React from "react";
-import Banner from "../Banner";
-import Courses from "../courses";
+
+
 import Card from "../Card";
 import PopularCategories from '../popularCategories'
 import InfoSection from "../InfoSection";
