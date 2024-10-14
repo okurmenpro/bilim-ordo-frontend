31037c705a4595691092e140351b4cc85d3c7adc
Okurmen Pro <okurmenpro@Bektens-MacBook-Pro.local>
2024-10-14 17:10:37 +0600

    import fix

--- src/components/HomePage/index.jsx
+++ src/components/HomePage/index.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import Banner from "../Banner";
 import Courses from "../courses";
 import Card from "../Card";
-import PopularCategories from "../PopularCategories/index";
+import PopularCategories from "../PopularCategories";
 import InfoSection from "../InfoSection";
 import TeachSection from "../TeachSection";
 
