78d09450f65fe65d7cebdfd9687c86435e43e14f
Tangsuluu <karybekovatansuluu303@gmail.com>
2024-10-14 18:27:48 +0600

    footer-media

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -7,15 +7,41 @@ import Volkswagen from "../../assets/images/volkswagen.png"
 import Box from "../../assets/images/box.png"
 import NetApp from "../../assets/images/netApp.png"
 import Eventbrite from "../../assets/images/eventbrite.png"
+import { FaLinkedinIn } from "react-icons/fa6";
+import { FaFacebookF } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
 
+
+const image  = [
+    {
+        image: ""
+    }
+]
 
 const Footer = () => {
   return (
-    <footer className='footer' >
+    <footer className='footer  active' >
         <div className='footer-business'>
+            <div className='top-footer-media'>
+                <strong>Follow us</strong>
+                <div className='footer_media_icons'>
+                    <span><FaLinkedinIn/></span>
+                    <span><FaFacebookF/></span>
+                    <span><FaTwitter/></span>
+                    <span><FaInstagram/></span>
+                </div>
+            </div>
+        <div className='lines'></div>
         <div className='footer-business2'>
          <div className='footer-business-strong'>
-                <strong>Top companies choose <span> Bilim-ordo Business</span> to build in-demand career skills. </strong>
+                <strong className='media-top'>Top companies choose <span> Bilim-ordo Business</span> to build in-demand career skills. </strong>
+                <div  className='media-code'>
+                    <span   id='media-icon'>
+                {<MdLanguage />}
+                    </span>
+                <strong className='media_top'> English</strong>
+                </div>
             </div>
             <div className='footer-business-img'>
              <img className='footer-png1' src={Nasdaq} alt="nasdaq" />
@@ -29,7 +55,7 @@ const Footer = () => {
         <div className='footer '>
             <div className='info-top'>        
        <div className='info-left'>
-       <div className='categori'>
+       <div className='categories'>
                         <ul>
                         <li>
                                 <a href="">Bilim-Ordo Business</a>
