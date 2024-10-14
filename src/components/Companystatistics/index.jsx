d83a9fcd84136ec453506a07292087f281ad98cb
Nurtilek <nuke.ff13@gmail.com>
2024-10-14 18:28:19 +0600

    js ачтым

--- src/components/Companystatistics/index.jsx
+++ src/components/Companystatistics/index.jsx
@@ -1,35 +1,10 @@
 import React from 'react';
 import "./Companystatistics.css";
+import { statisticsData } from '../../data/Statisticscompany';
 
 
 
 function Companystatistics() {
-  const statisticsData = [
-    {
-      id: 1,
-      value: "200+",
-      title: "Создание веб-сайтов",
-      description: "Мы создали веб-сайты, которые отвечают современным требованиям и эстетике.",
-    },
-    {
-      id: 2,
-      value: "97%",
-      title: "Удовлетворенность клиентов",
-      description: "Наши проекты понравились клиентам благодаря высокому качеству исполнения и вниманию.",
-    },
-    {
-      id: 3,
-      value: "34+",
-      title: "Члены команды",
-      description: "Мы объединяем таланты из разных областей, чтобы обеспечить высокий уровень услуг для наших клиентов.",
-    },
-    {
-      id: 4,
-      value: "100+",
-      title: "Удивительные клиенты",
-      description: "Их доверие и сотрудничество помогают нам расти и достигать новых высот.",
-    },
-  ];
   return (
     <div className='Companystatistics'>
       <h2>Нам доверяют более 16 000 компаний и миллионы учащихся по всему миру</h2>
