86462839ac678874dc2da1a13246b3d4605dbf03
MahmudovIbragim <ibrahimorunbaev59@gmail.com>
2024-10-14 15:00:13 +0600

    bug fix accardions

--- src/components/HomePage/index.jsx
+++ src/components/HomePage/index.jsx
@@ -1,10 +1,9 @@
-
 import React from "react";
 import Banner from "../Banner";
 import Courses from "../courses";
 
 import Card from "../Card";
-import PopularCategories from "../PopularCategories";
+import PopularCategories from "../popularCategories";
 import InfoSection from "../InfoSection";
 import TeachSection from "../TeachSection";
 
