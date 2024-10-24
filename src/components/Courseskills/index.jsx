1424fc6c836db92f32b0fcd51723c5b12353ee62
Nurtilek <nuke.ff13@gmail.com>
2024-10-24 14:14:46 +0600

    Компонеттердин аттарын озгорттум

---
+++ src/components/Courseskills/index.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import "./Courseskills.css";
+import { IoMdCheckmark } from "react-icons/io";
+import { skillsData } from '../../data/courseskills';
+
+
+
+function Pythonskills() {
+ 
+  return (
+    <div>
+      <div className="Course-skills">
+        <h2>Чему вы научитесь</h2>
+
+        <div className="skills-container">
+          {skillsData.map((skill) => (
+            <div className="skills" key={skill.id}>
+              <IoMdCheckmark />
+              <p>{skill.text}</p>
+            </div>
+          ))}
+        </div>
+
+      </div>
+    </div>
+  );
+}
+
+export default Pythonskills;
--- src/data/courseskills.js
+++ src/data/courseskills.js
@@ -1,11 +1,4 @@
-import React from 'react';
-import "./Pythonskills.css";
-import { IoMdCheckmark } from "react-icons/io";
-
-
-
-function Pythonskills() {
-  const skillsData = [
+export const skillsData = [
     { id: 1, text: "Писать простые программы на Python 3" },
     { id: 2, text: "Как писать простые игры типа крестиков-ноликов" },
     { id: 3, text: "Логика с условиями и циклами" },
@@ -15,23 +8,3 @@ function Pythonskills() {
     { id: 7, text: "Декораторы" },
     { id: 8, text: "Неизменяемые объекты" }
   ];
-  return (
-    <div>
-      <div className="Pythonskills">
-        <h1>Чему вы научитесь</h1>
-
-        <div className="skills-container">
-          {skillsData.map((skill) => (
-            <div className="skills" key={skill.id}>
-              <IoMdCheckmark />
-              <p>{skill.text}</p>
-            </div>
-          ))}
-        </div>
-
-      </div>
-    </div>
-  );
-}
-
-export default Pythonskills;
