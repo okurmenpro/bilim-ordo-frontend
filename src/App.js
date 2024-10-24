0e9fc20ee3e610e898ada47b572c1572e301c850
ErTy-Dev <erkutbekt@gmail.com>
2024-10-24 15:19:50 +0300

    add prettier

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
-import './App.css';
-import { RouterProvider } from 'react-router-dom';
-import { Router } from './router';
-import { CartProvider } from "./components/CartContext"; 
-
+import "./App.css";
+import { RouterProvider } from "react-router-dom";
+import { Router } from "./router";
+import { CartProvider } from "./components/CartContext";
 
 export default function App() {
-
-
-  return (
-    <CartProvider> 
-      <div className="App">
-        <RouterProvider router={Router} />
-      </div>
-    </CartProvider>
-  );
+	return (
+		<CartProvider>
+			<div className="App">
+				<RouterProvider router={Router} />
+			</div>
+		</CartProvider>
+	);
 }
