3b72b8c334860d54a8a13a1b8deb29b8cc0041be
Tangsuluu <karybekovatansuluu303@gmail.com>
2024-10-26 14:54:15 +0600

    solution_conflicts

--- src/components/HomePage/index.jsx
+++ src/components/HomePage/index.jsx
@@ -5,13 +5,9 @@ import Courseskills from "../Courseskills";
 
 
 
-import React from 'react'
-import Banner from '../Banner'
-import Courses from "../courses"
 import Companystatistics from "../Companystatistics"
 import CardContainer from '../CardContainer'
 import Card from "../Card";
-import PopularCategories from '../PopularCategories';
 import PopularCategories from "../popularCategories";
 import InfoSection from "../InfoSection";
 import TeachSection from "../TeachSection";
