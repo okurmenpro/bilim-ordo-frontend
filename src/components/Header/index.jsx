31045a431fc584ef28da9830e87b890eef5e7680
Islam <gojo060809@gmail.com>
2024-10-26 18:06:17 +0600

    fixed header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,28 +1,23 @@
-import  {React, useContext, useEffect, useState} from "react";
+import { React, useContext, useEffect, useState } from "react";
 import "./Header.scss";
 import cart from '../../assets/svg/cart.svg'
-import earth from '../../assets/svg/earth.svg'
 import { GrSearch } from "react-icons/gr";
 import Dropdown from '../Dropdown/Index'
 import { NavLink } from 'react-router-dom';
-import { CartContext} from '../CartContext'
+import { CartContext } from '../CartContext'
+
 function Header() {
-  const providers = ['Udemy Business', 'Преподавайте на Udemy']
   const { cartItems } = useContext(CartContext);
   const [inputWidth] = useState("200px");
   return (
     <div className="aback">
-      <header className="head ">
+      <header className="head">
         <div>
           <NavLink to='/'>
             <h2><span>B</span>ILIM-ORDO</h2>
           </NavLink>
         </div>
         <Dropdown id="dropdowns" />
-        <div className="buttons">
-          {providers.map((provider) =>
-            <button id="active-btn">{provider}</button>)}
-        </div>
         <div id="search">
           <div class="input-group mb-3">
             <input class="form-control" placeholder="Search" style={{ width: inputWidth }} aria-label="Recipient's username" aria-describedby="basic-addon2" />
@@ -33,18 +28,17 @@ function Header() {
             </div>
           </div>
         </div>
+        <NavLink to='/basket'>
+          <div className="icon">
+            <img src={cart} alt="Cart" />
+            {cartItems.length > 0 && (
+            <span className="cart-count">{cartItems.length}</span>
+            )}
+          </div>
+          </NavLink>
         <div className="buttons">
           <button className="login">Войти</button>
           <button className="sign-up">Зарегистрироваться</button>
-        </div>
-        <div className="icons">
-          <NavLink to='/basket'>
-            <img src={cart} alt="Cart" />
-            {cartItems.length > 0 && (
-              <span className="cart-count">{cartItems.length}</span>
-            )}
-          </NavLink>
-          <img src={earth} alt="" />
         </div>
       </header>
     </div>
