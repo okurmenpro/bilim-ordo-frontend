f1b21428eb0a826c8185e878c2823f91bea27c59
ela <jenni989812@gmail.com>
2024-11-04 17:34:26 +0600

    page

--- src/components/HomePage/index.jsx
+++ src/components/HomePage/index.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import Banner from '../Banner'
-import Courses from "../Courses"
+import Courses from '../courses/'
 import Companystatistics from "../Companystatistics"
 import CardContainer from '../CardContainer'
 import Card from "../Card";
-import PopularCategories from "../PopularCategories";
+import PopularCategories from "../popularCategories/index";
 import InfoSection from "../InfoSection";
 import TeachSection from "../TeachSection";
 
