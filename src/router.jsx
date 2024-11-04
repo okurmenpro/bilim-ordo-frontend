c6fbb427efebf2a811f589c473f44c4c06921560
saadat <devilwq99@gmail.com>
2024-11-04 18:48:35 +0600

    l;kkjhgfd

--- src/router.jsx
+++ src/router.jsx
@@ -6,11 +6,8 @@ import HomePage from "./components/HomePage";
 import Sidebar from "./components/Sidebar";
 import TeacherProfile from "./components/TeacherProfile";
 import SignUp from "./components/SignUp";
-<<<<<<< HEAD
 import TeachersCourses from "./components/TeachersCourses";
-=======
 import Loginn from "./pages/Login";
->>>>>>> dev
 import TeacherPage from "./pages/TeacherPage";
 export const Router = createBrowserRouter([
   {
