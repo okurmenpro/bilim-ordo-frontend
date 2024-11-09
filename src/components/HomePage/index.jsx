b571904dbebfd5419f8a24e03d0ab5a25ccd1946
Marlis <belekowmarlis21@gmail.com>
2024-11-09 18:24:50 +0300

    faq

--- src/components/HomePage/index.jsx
+++ src/components/HomePage/index.jsx
@@ -1,13 +1,10 @@
-import React from 'react'
-import Banner from '../Banner'
-import Courses from '../courses/'
-import Companystatistics from "../Companystatistics"
-import CardContainer from '../CardContainer'
+import React from "react";
+import Banner from "../Banner";
 import Courses from "../Courses";
 import Companystatistics from "../Companystatistics";
 import CardContainer from "../CardContainer";
 import Card from "../Card";
-import PopularCategories from "../popularCategories/index";
+import PopularCategories from "../PopularCategories/index";
 import InfoSection from "../InfoSection";
 import TeachSection from "../TeachSection";
 
