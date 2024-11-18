11727708d167a602c6f2d93bf049fd99e3c60c70
elaman2323 <you@example.com>
2024-11-18 13:13:22 +0300

    rewiev-adaptiv-ipad

--- src/components/Request/index.jsx
+++ src/components/Request/index.jsx
@@ -1,18 +1,18 @@
-import "./Request.scss"
+    import "./Request.scss"
 
-const Request = () => {
-    return (
-        <div className="leave-request">
-            <h4 className="leave-req-title">Оставить заявку</h4>
-            <div className="inf">
-                <input type="text" placeholder='Имя' />
-                <input type="text" placeholder='Фамилия' />
-                <input type="text" placeholder='Gmail' />
-                <input type="text" placeholder='Номер телефона' />
-                <button className="leave-req-btn">Оставить заявку</button>
+    const Request = () => {
+        return (
+            <div className="leave-request">
+                <h4 className="leave-req-title">Оставить заявку</h4>
+                <div className="inf">
+                    <input type="text" placeholder='Имя' />
+                    <input type="text" placeholder='Фамилия' />
+                    <input type="text" placeholder='Gmail' />
+                    <input type="text" placeholder='Номер телефона' />
+                    <button className="leave-req-btn">Оставить заявку</button>
+                </div>
             </div>
-        </div>
-    )
-}
+        )
+    }
 
-export default Request
+    export default Request
