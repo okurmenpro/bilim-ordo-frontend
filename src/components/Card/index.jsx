1d53c1c5a9e2fe4a594b33d4b01a0ada210140a4
KazbekovAktenir <kazbekovaktenir@gmail.com>
2024-11-19 17:55:24 +0600

    debug add API

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -16,7 +16,7 @@ function Card() {
   }, []);
 
   useEffect(() => {
-    console.log(courses);
+    console.log("Courses:", courses);
   }, [courses]);
 
   const scrollLeft = () => {
@@ -41,7 +41,9 @@ function Card() {
 
       <div className="flex" ref={scrollRef}>
         {Array.isArray(courses) && courses.length > 0 ? (
-          courses.map((elem) => <CardContainer key={elem.id} elem={elem} />)
+          courses.map((elem) =>
+            elem ? <CardContainer key={elem.id} elem={elem} /> : null
+          )
         ) : (
           <p>Курсы загружаются...</p>
         )}
@@ -55,105 +57,3 @@ function Card() {
 }
 
 export default Card;
-
-// import React, { useContext, useEffect, useState, useRef } from "react";
-// import axios from "axios";
-// import "./Card.css";
-// import { MdOutlineStarPurple500 } from "react-icons/md";
-// import { CartContext } from "../CartContext";
-// import { NavLink } from "react-router-dom";
-// import { course } from "../../data/Card";
-// import {
-//   IoIosArrowDropleftCircle,
-//   IoIosArrowDroprightCircle,
-// } from "react-icons/io";
-// function Card() {
-//   const { addToCart, cartItems } = useContext(CartContext);
-//   const [cardCourse, setCourse] = useState(course);
-//   const scrollRef = useRef(null);
-//   const getcourse = async () => {
-//     try {
-//       const response = await axios.get("/course");
-//       setCourse(response.data);
-//     } catch (error) {
-//       console.error(error);
-//     }
-//   };
-
-//   useEffect(() => {
-//     getcourse();
-//   }, []);
-
-//   const handleAddToCart = (item) => {
-//     const isAlreadyInCart = cartItems.find(
-//       (cartItem) => cartItem.id === item.id
-//       // (cartItem) => cartItem.name === item.name
-//     );
-//     if (!isAlreadyInCart) {
-//       addToCart(item);
-//     }
-//   };
-
-//   const scrollLeft = () => {
-//     scrollRef.current.scrollBy({
-//       left: -scrollRef.current.clientWidth,
-//       behavior: "smooth",
-//     });
-//   };
-//   const scrollRight = () => {
-//     scrollRef.current.scrollBy({
-//       left: scrollRef.current.clientWidth,
-//       behavior: "smooth",
-//     });
-//   };
-//   const truncateText = (text, limit) => {
-//     return text.length > limit ? text.substring(0, limit) + "..." : text;
-//   };
-//   return (
-//     <div className="card-container">
-//       <button onClick={scrollLeft} className="scroll-button left">
-//         <IoIosArrowDropleftCircle size={30} />
-//       </button>
-//       <div className="flex" ref={scrollRef}>
-//         {cardCourse.map((item, index) => {
-//           const isInCart = cartItems.some(
-//             (cartItem) => cartItem.id === item.id
-//           );
-//           return (
-//             <div key={index} className="cart">
-//               <NavLink to="/course">
-//                 <img src={item.img} alt={item.name} />
-//                 <h2>{truncateText(item.name, 30)}</h2>
-//                 <h3>{item.author}</h3>
-//                 <div className="cart-flex">
-//                   <h2>4,3</h2>
-//                   <MdOutlineStarPurple500 />
-//                   <MdOutlineStarPurple500 />
-//                   <MdOutlineStarPurple500 />
-//                   <MdOutlineStarPurple500 />
-//                   <MdOutlineStarPurple500 />
-//                   <p>(3 3316)</p>
-//                 </div>
-//                 <div className="cart-top">
-//                   <p>{item.price}</p>
-//                 </div>
-//               </NavLink>
-//               <button
-//                 onClick={() => handleAddToCart(item)}
-//                 className="add-to-cart-button"
-//                 disabled={isInCart}
-//               >
-//                 {isInCart ? "В корзине" : "Добавить в корзину"}
-//               </button>
-//             </div>
-//           );
-//         })}
-//       </div>
-//       <button onClick={scrollRight} className="scroll-button right">
-//         <IoIosArrowDroprightCircle size={30} />
-//       </button>
-//     </div>
-//   );
-// }
-
-// export default Card;
--- src/components/CardContainer/index.jsx
+++ src/components/CardContainer/index.jsx
@@ -1,33 +1,16 @@
 import React from "react";
+import "./Container.css";
 
 const CardContainer = ({ elem }) => {
-  console.log(elem);
+  // console.log(elem);
+  console.log("elem:", JSON.stringify(elem, null, 2));
   return (
     <div className="cart-1 container">
       <h2 style={{ marginRight: "800px", fontWeight: "700", fontSize: "30px" }}>
-        title: {elem.title};{" "}
+        title: {JSON.stringify(elem, null, 2)};
       </h2>
+      {/* <p style={{ fontSize: "16px", color: "#555" }}>{elem.description}</p> */}
     </div>
   );
 };
-
 export default CardContainer;
-
-// import "./Container.css";
-
-// function CardContainer({ name }) {
-//   if (!name) {
-//     return <p>Курс не найден</p>;
-//   }
-//   console.log(name.title);
-
-//   return (
-//     <div className="cart-1 container">
-//       <h2 style={{ marginRight: "800px", fontWeight: "700", fontSize: "30px" }}>
-//         Title: {name.title}
-//       </h2>
-//     </div>
-//   );
-// }
-
-// export default CardContainer;
