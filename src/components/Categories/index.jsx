75ac15db79fa197bc40b7a85739a3ca7668636f0
Marlis <belekowmarlis21@gmail.com>
2024-11-21 17:38:24 +0300

    categories

---
+++ src/components/Categories/index.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import "./Categories.scss";
+import astrology from "../../assets/svg/astrology.svg";
+import astrology1 from "../../assets/svg/astrology1.svg";
+import astrology2 from "../../assets/svg/astrology2.svg";
+import astrology3 from "../../assets/svg/astrology3.svg";
+
+function index() {
+  return (
+    <div className="categories container">
+      <div className="categoriesh2">
+        <h2>Top Categories</h2>
+        <h6>Label</h6>
+      </div>
+
+      <div className="astrology ">
+        <div className="astrology-cours">
+          <img src={astrology} alt="" />
+          <span className="span">Astrology</span>
+          <span className="span1">11 Courses</span>
+        </div>
+        <div className="astrology-cours">
+          <img src={astrology1} alt="" />
+          <span className="span">Astrology</span>
+          <span className="span1">11 Courses</span>
+        </div>
+        <div className="astrology-cours">
+          <img src={astrology2} alt="" />
+          <span className="span">Astrology</span>
+          <span className="span1">11 Courses</span>
+        </div>
+        <div className="astrology-cours">
+          <img src={astrology3} alt="" />
+          <span className="span">Astrology</span>
+          <span className="span1">11 Courses</span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default index;
