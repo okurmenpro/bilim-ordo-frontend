71e20331f0806b3d937062d26bccaf207eaf1d3b
elaman2323 <you@example.com>
2024-11-25 13:00:26 +0300

    new-rating

--- src/components/Companystatistics/index.jsx
+++ src/components/Companystatistics/index.jsx
@@ -16,7 +16,7 @@ function Companystatistics() {
     if (ref.current) {
       observer.observe(ref.current);
     }
-
+    
     return () => {
       if (ref.current) {
         observer.unobserve(ref.current);
