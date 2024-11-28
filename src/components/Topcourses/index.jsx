b2288933f33a1f67fae203aeb6ee16d17192c6a5
Marlis <belekowmarlis21@gmail.com>
2024-11-28 18:14:30 +0300

    Topcourses2

--- src/components/Topcourses/index.jsx
+++ src/components/Topcourses/index.jsx
@@ -1,11 +1,12 @@
 import React, { useState, useEffect, useRef } from "react";
-import "./topcourses.scss";
+import "./Topcourses.scss";
 import { MdOutlineStarPurple500 } from "react-icons/md";
 import { topcourse } from "../../data/topcourse";
 import {
   IoIosArrowDropleftCircle,
   IoIosArrowDroprightCircle,
 } from "react-icons/io";
+import { Link } from "react-router-dom";
 
 function TopCourses() {
   const scrollRef = useRef(null);
@@ -53,24 +54,29 @@ function TopCourses() {
 
         {topcourses.map((course) => (
           <div className="design" key={course.id}>
-            <img src={course.image} alt={course.title} />
-            <div className="design1">
-              <h2>{course.title}</h2>
-              <h3>{course.author}</h3>
-              <div className="design-icon">
-                <div className="line-ratings">
-                  {[...Array(5)].map((_, index) => (
-                    <MdOutlineStarPurple500 className="linestart" key={index} />
-                  ))}
-                  <p className="ratings">({course.ratings} Ratings)</p>
+            <Link to="course">
+              <img src={course.image} alt={course.title} />
+              <div className="design1">
+                <h2>{course.title}</h2>
+                <h3>{course.author}</h3>
+                <div className="design-icon">
+                  <div className="line-ratings">
+                    {[...Array(5)].map((_, index) => (
+                      <MdOutlineStarPurple500
+                        className="linestart"
+                        key={index}
+                      />
+                    ))}
+                    <p className="ratings">({course.ratings} Ratings)</p>
+                  </div>
+                  <p className="ratings2">
+                    {course.totalHours} Total Hours. {course.lectures} Lectures.{" "}
+                    {course.level}
+                  </p>
+                  <p className="price1">{course.price}</p>
                 </div>
-                <p className="ratings2">
-                  {course.totalHours} Total Hours. {course.lectures} Lectures.{" "}
-                  {course.level}
-                </p>
-                <p className="price1">{course.price}</p>
               </div>
-            </div>
+            </Link>
           </div>
         ))}
 
