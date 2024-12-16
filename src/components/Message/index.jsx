56151a34309c9ea5054d3cc72ae074242a97f2ba
Nurtilek <nuke.ff13@gmail.com>
2024-12-16 17:47:43 +0600

    cardcontainernew

--- src/components/Message/index.jsx
+++ src/components/Message/index.jsx
@@ -1,50 +1,56 @@
-import React from 'react'
-import "./Message.scss"
-import Autor from "../../assets/images/autor.png"
-import Autor2 from "../../assets/images/tacher2.png"
-import Courses from "../Courses"
-import { NavLink } from 'react-router-dom'
+import React from "react";
+import "./Message.scss";
+import Autor from "../../assets/images/autor.png";
+import Autor2 from "../../assets/images/tacher2.png";
+import Courses from "../Courses";
+import { NavLink } from "react-router-dom";
 
 function MessagePage() {
-    return (
-        <section className='section'>
-        <Courses />
-        <div className='direct'>
-             <NavLink to="/profilemessageteachers">
-            <div className='first-direct'>
-                <div className='info-direct'>
-                    <img src={Autor} alt="" />
-                    <div className='date-direct'>
-                        <h1>Ronald Richards</h1>
-                        <p>18th March, 2024</p>
-                    </div>
-                </div>
-                <strong>Thank you for asking your doubt, I’ll send you a pdf file which cover the problems you are facing. If you have any...</strong>
+  return (
+    <section className="section">
+      <Courses />
+      <div className="direct">
+        <NavLink to="/profilemessageteachers">
+          <div className="first-direct">
+            <div className="info-direct">
+              <img src={Autor} alt="" />
+              <div className="date-direct">
+                <h1>Ronald Richards</h1>
+                <p>18th March, 2024</p>
+              </div>
             </div>
-             </NavLink>
-            <div className='first-direct' >
-                <div className='info-direct'>
-                    <img className='Autor2' src={Autor2} alt="" />
-                    <div className='date-direct'>
-                        <h1>Devon Lane</h1>
-                        <p>18th March, 2024</p>
-                    </div>
-                    <strong>Thank you for asking your doubt, I’ll send you a pdf file which cover the problems you are facing. If you have any...</strong>
-                </div>
-                <div className='first-direct' >
-                    <div className='info-direct'>
-                        <img className='Autor2' src={Autor2} alt="" />
-                        <div className='date-direct'>
-                            <h1>Devon Lane</h1>
-                            <p>18th March, 2024</p>
-                        </div>
-                    </div>
-                    <strong>I’ll Get back to you as soon as possbile.</strong>
-                </div>
+            <strong>
+              Thank you for asking your doubt, I’ll send you a pdf file which
+              cover the problems you are facing. If you have any...
+            </strong>
+          </div>
+        </NavLink>
+        <div className="first-direct">
+          <div className="info-direct">
+            <img className="Autor2" src={Autor2} alt="" />
+            <div className="date-direct">
+              <h1>Devon Lane</h1>
+              <p>18th March, 2024</p>
             </div>
+            <strong>
+              Thank you for asking your doubt, I’ll send you a pdf file which
+              cover the problems you are facing. If you have any...
+            </strong>
+          </div>
+          <div className="first-direct">
+            <div className="info-direct">
+              <img className="Autor2" src={Autor2} alt="" />
+              <div className="date-direct">
+                <h1>Devon Lane</h1>
+                <p>18th March, 2024</p>
+              </div>
             </div>
-        </section>
-    )
+            <strong>I’ll Get back to you as soon as possbile.</strong>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
-export default MessagePage
+export default MessagePage;
