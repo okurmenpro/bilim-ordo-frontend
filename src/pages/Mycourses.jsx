e3c577c6f9f51ddc52ad8966edaf70fcb027f99a
alisher <alisermanasov00@gmail.com>
2024-12-18 01:17:21 +0600

    fix login

---
+++ src/pages/Mycourses.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import TeachersCourses from '../components/TeachersCourses'
+
+function Mycourses() {
+  return (
+    <div>
+      <TeachersCourses />
+    </div>
+  )
+}
+
+export default Mycourses
