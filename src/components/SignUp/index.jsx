2d3754e9782005ba7d36736189740bb21ac0f626
Islam <gojo060809@gmail.com>
2024-12-17 18:11:22 +0600

    add singup

--- src/components/SignUp/index.jsx
+++ src/components/SignUp/index.jsx
@@ -1,14 +1,73 @@
-import React from "react";
+import React, { useState } from "react";
 import { IoChevronBackOutline } from "react-icons/io5";
 import "./SignUp.scss";
 import signup from "../../assets/images/signup2.png";
 import facebook from "../../assets/svg/facebook.svg";
 import google from "../../assets/svg/googel.svg";
 import microsoft from "../../assets/svg/microsoft.svg";
-
 import { FaArrowRightLong } from "react-icons/fa6";
 import { NavLink } from "react-router-dom";
-const index = () => {
+import axios from "axios";
+
+const SignUp = () => {
+  const [userParams, setUserParams] = useState({
+    email: "",
+    password: "",
+    name: "",
+    avatar: "",
+  });
+
+  // Ошол эле учурда сырсөздөрдү салыштыруу үчүн дагы бир state
+  // const [confirmPassword, setConfirmPassword] = useState("");
+
+  // Серверге маалымат жөнөтүү функциясы
+  async function hold(userDataParams) {
+    try {
+      const { data } = await axios.post(
+        "https://api.escuelajs.co/api/v1/users/",
+        userDataParams
+      );
+
+      const loginResponse = await axios.post(
+        "https://api.escuelajs.co/api/v1/auth/login",
+        {
+          email: userDataParams.email,
+          password: userDataParams.password,
+        }
+      );
+
+      // Колдонуучунун маалыматын localStorage'га сактоо
+      localStorage.setItem("user", JSON.stringify(loginResponse.data));
+      console.log(data, "Registration successful");
+    } catch (error) {
+      console.error("Error occurred:", error.response?.data || error.message);
+      alert(
+        "Error: " +
+          (error.response?.data?.message || "Something went wrong, try again!")
+      );
+    }
+  }
+
+  // Инпутту өзгөртүү функциясы
+  const changeInput = (e) => {
+    const { name, value } = e.target;
+    setUserParams({ ...userParams, [name]: value });
+  };
+
+  // Форманы жөнөтүү жана текшерүү
+  const sendForm = (e) => {
+    e.preventDefault();
+
+    // Сырсөздөрдү салыштыруу
+  //   if (userParams.password !== confirmPassword) {
+  //     alert("Passwords do not match!");
+  //     return;
+  //   }
+
+    console.log(userParams);
+    hold(userParams);
+  };
+
   return (
     <div>
       <div className="step-back">
@@ -17,93 +76,97 @@ const index = () => {
         </NavLink>
       </div>
       <div className="main-spot">
-          <img className="signup-png" src={signup} alt="" />
-          <div className="right-register">
-            <h1 className="registration">Create Your Account</h1>
+        <img className="signup-png" src={signup} alt="" />
+        <div className="right-register">
+          <h1 className="registration">Create Your Account</h1>
 
-            <div className="inputs">
-              <h3>Full Name</h3>
-              <div className="full-name-input">
-                <input type="text" placeholder="First Name" />
-                <input type="text" placeholder="Last Name" />
-              </div>
-            </div>
-
+          <form onSubmit={sendForm}>
             <div className="Username-input">
               <h3>Username</h3>
-              <input type="text" placeholder="Username" />
+              <input
+                onChange={changeInput}
+                type="text"
+                placeholder="Name"
+                name="name"
+              />
             </div>
 
             <div className="Email-input">
               <h3>Email</h3>
-              <input type="text" placeholder="Email ID" />
+              <input
+                onChange={changeInput}
+                type="email"
+                placeholder="Email"
+                name="email"
+              />
+            </div>
+
+            <div className="Avatar-input">
+              <h3>Avatar URL</h3>
+              <input
+                onChange={changeInput}
+                type="text"
+                placeholder="Avatar URL"
+                name="avatar"
+              />
             </div>
 
             <div className="password-container">
               <div className="password-input">
                 <h3>Password</h3>
-                <input type="text" placeholder="Enter Password" />
+                <input
+                  onChange={changeInput}
+                  type="password"
+                  placeholder="Password"
+                  name="password"
+                />
               </div>
 
               <div className="password-input">
                 <h3>Confirm Password</h3>
-                <input type="text" placeholder="Confirm password" />
+                <input
+                  // onChange={(e) => setConfirmPassword(e.target.value)}
+                  type="password"
+                  placeholder="Confirm Password"
+                />
               </div>
             </div>
+
             <div className="buttom-label">
-              <button>
-                <p>Label</p>
-                <FaArrowRightLong className="icon-right" />
-              </button>
+              <input type="submit" value="Create an account" />
             </div>
+          </form>
 
-            <div className="Sign-up-with">
-              <div className="left-width"></div>
-              <p>Sign up with</p>
-              <div className="left-width"></div>
-            </div>
+          <div className="Sign-up-with">
+            <div className="left-width"></div>
+            <p>Sign up with</p>
+            <div className="left-width"></div>
+          </div>
 
-            <div className="fgm-sign-in">
-              <button className="Facebook-google-microsoft">
-                <div>
-                  <img src={facebook} alt="" />
-                  <p
-                    style={{
-                      color: "blue"
-                    }}>
-                    Facebook
-                  </p>
-                </div>
-              </button>
-              <button className="Facebook-google-microsoft">
-                <div>
-                  <img src={google} alt="" />
-                  <p
-                    style={{
-                      color: " rgb(234, 67, 53)",
-                    }}
-                  >
-                    Google
-                  </p>
-                </div>
-              </button>
-              <button className="Facebook-google-microsoft">
-                <div>
-                  <img src={microsoft} alt="" />
-                  <p
-                    style={{
-                      color: "rgb(0, 0, 0)",
-                    }}
-                  >
-                    Microsoft
-                  </p>
-                </div>
-              </button>
-            </div>
+          <div className="fgm-sign-in">
+            <button className="Facebook-google-microsoft">
+              <div>
+                <img src={facebook} alt="" />
+                <p style={{ color: "blue" }}>Facebook</p>
+              </div>
+            </button>
+            <button className="Facebook-google-microsoft">
+              <div>
+                <img src={google} alt="" />
+                <p style={{ color: " rgb(234, 67, 53)" }}>Google</p>
+              </div>
+            </button>
+            <button className="Facebook-google-microsoft">
+              <div>
+                <img src={microsoft} alt="" />
+                <p style={{ color: "rgb(0, 0, 0)" }}>Microsoft</p>
+              </div>
+            </button>
           </div>
         </div>
+      </div>
     </div>
   );
 };
 
-export default index;
+export default SignUp;
