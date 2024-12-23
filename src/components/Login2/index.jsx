11cea07e12fc6203c6fefc4c31fc8bd74fcf8573
Nurtilek <nuke.ff13@gmail.com>
2024-12-23 16:28:00 +0600

    sinupadap

--- src/components/Login2/index.jsx
+++ src/components/Login2/index.jsx
@@ -58,20 +58,32 @@ const Authorization = () => {
         </div>
         <button className="login-button" onClick={handleLogin}>
           Войти
-          <svg className="arrow-right" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
-            <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8" />
+          <svg
+            className="arrow-right"
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            class="bi bi-arrow-right"
+            viewBox="0 0 16 16"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8"
+            />
           </svg>
-
         </button>
 
         <div className="Signupwith">
-          <div className="line1"> <hr className="here2" /></div>
+          <div className="line1">
+            {" "}
+            <hr className="here2" />
+          </div>
           <p className="words"> Sign With In</p>
-          <div className="Line2"><hr className="here" /></div>
-
+          <div className="Line2">
+            <hr className="here" />
+          </div>
         </div>
-
-
 
         <div className="fgm-sign-in">
           <button className="Facebook-google-microsoft">
@@ -79,8 +91,9 @@ const Authorization = () => {
               <img src={facebook} alt="" />
               <p
                 style={{
-                  color: "blue"
-                }}>
+                  color: "blue",
+                }}
+              >
                 Facebook
               </p>
             </div>
@@ -114,7 +127,6 @@ const Authorization = () => {
       <div className="authorization-left">
         <img className="big-images" src={authorization} alt="Authorization" />
       </div>
-
     </div>
   );
 };
