c5d1bf2e91db73da07a5210617af352ca5d6de5e
Nurtilek <nuke.ff13@gmail.com>
2024-12-27 03:27:30 +0600

    home page

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Carousel } from "react-bootstrap";
 import slides from "./Carousel.json";
 import "./Carousel.scss";
