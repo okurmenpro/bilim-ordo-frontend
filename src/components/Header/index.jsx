02a4084985aa7fc62b75a4876371ed5ecec45e49
Nurtilek <nuke.ff13@gmail.com>
2025-01-02 14:41:43 +0600

    registernew

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -92,7 +92,7 @@ function Header() {
               {/* "Войти" changed to "Login" */}
             </NavLink>
             <NavLink to="/signup">
-              <button className="sign-up">SignUp</button>{" "}
+              <button className="sign-up">Sign Up</button>{" "}
               {/* "Зарегистрироваться" changed to "Sign Up" */}
             </NavLink>
           </div>
