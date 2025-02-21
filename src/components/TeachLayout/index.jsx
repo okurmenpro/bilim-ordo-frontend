2339d9102035caf7ce4c70881427ea84a1fcfca8
alisher <alisermanasov00@gmail.com>
2025-02-22 00:25:37 +0600

    buto elek instructor page

--- src/components/TeachLayout/index.jsx
+++ src/components/TeachLayout/index.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Instructor from '../Instructor'
 import { Outlet } from 'react-router'
+import TeacHeader from '../TeacHeader'
 
 function TeachLayout() {
   return (
     <div>
       <Instructor />
+      <TeacHeader />
       <Outlet />
     </div>
   )
